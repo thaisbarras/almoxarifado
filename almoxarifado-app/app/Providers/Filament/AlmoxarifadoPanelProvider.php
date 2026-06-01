c0de28eb6dc7a345a7d4b48d49f6d082167c45db
ThaisBarras <thaisbarras@hotmail.com>
2026-06-01 11:55:38 -0300

    Criação dos Models, Migrates e Resources Laravel e Filament

--- almoxarifado-app/app/Providers/Filament/AlmoxarifadoPanelProvider.php
+++ almoxarifado-app/app/Providers/Filament/AlmoxarifadoPanelProvider.php
@@ -30,10 +30,7 @@ class AlmoxarifadoPanelProvider extends PanelProvider
             ->login()
             ->colors([
                 'primary' => Color::Amber,
-            ])
-            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
-            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
-            ->pages([
+                
                 Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
